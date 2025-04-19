789416bf5f7c2a43ccfccbeba8cb01660e066208
aymeriick <aymerickcensier@gmail.com>
2025-04-19 18:14:34 +1200

    removed css

--- rair/src/App.tsx
+++ rair/src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css'
+import Header from './components/Header.tsx'
+import Footer from './components/Footer.tsx'
 
 function App() {
-
+    return(
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App
