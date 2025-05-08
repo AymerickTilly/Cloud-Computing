c8a8128c4fb4232dbf8c899e08c5b91f477149f3
aymeriick <aymerickcensier@gmail.com>
2025-05-08 15:08:30 +1200

    Signup schema

---
+++ rair/src/schemas/TsignUpSchemas.tsx
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+    username: z.string().min(4),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(10, "Password must be at least 10 characters"),
+    confirmpassword: z.string().min(10, "Confirm Password must be at least 10 characters"),
+  });
+  
+  export type TsignUpSchema = z.infer<typeof signUpSchema>;
