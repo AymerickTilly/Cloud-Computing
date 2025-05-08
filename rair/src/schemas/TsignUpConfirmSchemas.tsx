b9dbf47fe73f2ad8977a477b7a1aca50302a7373
aymeriick <aymerickcensier@gmail.com>
2025-05-09 09:08:16 +1200

    confirmSignUp

--- rair/src/schemas/TsignUpConfirmSchemas.tsx
+++ rair/src/schemas/TsignUpConfirmSchemas.tsx
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const signUpConfirmSchema = z.object({
     username: z.string().min(4),
-    code: z.number(),
+    code: z.string(),
   });
   
   export type TsignUpConfirmSchema = z.infer<typeof signUpConfirmSchema>;
