ffb1ad7f1aa042e7cdd87994325be71a287ac099
aymeriick <aymerickcensier@gmail.com>
2025-05-09 22:24:51 +1200

    Change routing method

--- rair/src/App.tsx
+++ rair/src/App.tsx
@@ -13,8 +13,10 @@ import NavigationBar from './components/Navbar';
 import PrivateRoutes from './routes/PrivateRoutes';
 import ProtectedRoutes from './routes/ProtectedRoutes';
 import ConfirmRegisterForm from './pages/ConfirmRegister';
+import ConfirmRegisterRoute from './routes/ConfirmRegisterRoute';
 
 const App = () => {
+
   return (
     <>
       <BrowserRouter>
@@ -22,13 +24,16 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<RegisterForm />} />
-          <Route path="/confirmRegister" element={<ConfirmRegisterForm />} />
+
+          
           <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+
+          <Route element={<ConfirmRegisterRoute/>}>
+          <Route path="/confirmRegister" element={<ConfirmRegisterForm />} />
+          </Route>
 
           <Route element={<ProtectedRoutes />}>
-            <Route path="/shop" element={<Shop />} />
-
-
             <Route element={<PrivateRoutes allowedGroups={['Admin']} />}>
               <Route path="/adminboard" element={<AdminPage />} />
             </Route>
