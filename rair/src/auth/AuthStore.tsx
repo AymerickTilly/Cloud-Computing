744b29fa2cd5456d23992739b74269fca9a9fd79
aymeriick <aymerickcensier@gmail.com>
2025-05-09 22:24:38 +1200

    pendingUsername, resetAuth

--- rair/src/auth/AuthStore.tsx
+++ rair/src/auth/AuthStore.tsx
@@ -1,32 +1,37 @@
+// In AuthStore.ts
 import { create } from 'zustand';
 
-interface IAuth {
+type AuthStore = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  user: any | null;
+  user: any;
+  loading: boolean;
   token: string | null;
   email: string | null;
   groups: string[];
-  loading: boolean;
+  pendingUsername: string | null;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  setUser: (user: any | null) => void;
+  setUser: (user: any) => void;
+  setLoading: (loading: boolean) => void;
   setToken: (token: string | null) => void;
   setEmail: (email: string | null) => void;
   setGroups: (groups: string[]) => void;
-  setLoading: (loading: boolean) => void;
+  setPendingUsername: (username: string | null) => void;
   resetAuth: () => void;
-}
+};
 
-export const useAuthStore = create<IAuth>((set) => ({
+export const useAuthStore = create<AuthStore>((set) => ({
   user: null,
+  loading: false,
   token: null,
   email: null,
   groups: [],
-  loading: false,
+  pendingUsername: null,
   setUser: (user) => set({ user }),
+  setLoading: (loading) => set({ loading }),
   setToken: (token) => set({ token }),
   setEmail: (email) => set({ email }),
   setGroups: (groups) => set({ groups }),
-  setLoading: (loading) => set({ loading }),
+  setPendingUsername: (username) => set({ pendingUsername: username }),
   resetAuth: () =>
     set({
       user: null,
