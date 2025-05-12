af574af2514aaf993b3826eb662840db740612ae
aymeriick <aymerickcensier@gmail.com>
2025-05-12 13:49:54 +1200

    Remember me boolean added

--- rair/src/schemas/TsignInSchemas.tsx
+++ rair/src/schemas/TsignInSchemas.tsx
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const signInSchema = z.object({
     email: z.string().email("Invalid email address"),
     password: z.string().min(10, "Password must be at least 10 characters"),
+    remeberme: z.boolean().default(false),
   });
   
   export type TsignInSchema = z.infer<typeof signInSchema>;
