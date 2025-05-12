8ad336f5f0ffd422f4feb561c704da6580eb6c2e
aymeriick <aymerickcensier@gmail.com>
2025-05-12 13:48:15 +1200

    verifying passwords matching

--- rair/src/schemas/TsignUpSchemas.tsx
+++ rair/src/schemas/TsignUpSchemas.tsx
@@ -1,9 +1,13 @@
 import { z } from "zod";
 
 export const signUpSchema = z.object({
-    username: z.string().email("Invalid email address"),
-    password: z.string().min(10, "Password must be at least 10 characters"),
-    confirmpassword: z.string().min(10, "Confirm Password must be at least 10 characters"),
+    username: z.string().email('Invalid email address'),
+    password: z.string().min(8, 'Password must be at least 8 characters'),
+    confirmpassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmpassword, {
+    message: 'Passwords must match',
+    path: ['confirmpassword'],
   });
   
   export type TsignUpSchema = z.infer<typeof signUpSchema>;
