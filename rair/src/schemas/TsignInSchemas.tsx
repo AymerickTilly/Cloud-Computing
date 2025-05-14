a73ab5a65e299f893b703ca5a231b4c124079856
aymeriick <aymerickcensier@gmail.com>
2025-05-14 13:02:17 +1200

    removed min 10 characters to 8

--- rair/src/schemas/TsignInSchemas.tsx
+++ rair/src/schemas/TsignInSchemas.tsx
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const signInSchema = z.object({
     email: z.string().email("Invalid email address"),
-    password: z.string().min(10, "Password must be at least 10 characters"),
+    password: z.string(),
   });
   
   export type TsignInSchema = z.infer<typeof signInSchema>;
--- rair/src/schemas/TsignUpConfirmSchemas.tsx
+++ rair/src/schemas/TsignUpConfirmSchemas.tsx
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const signUpConfirmSchema = z.object({
     email: z.string().email("Invalid email address"),
     code: z.string(),
-    password: z.string().min(10, "Password must be 10 characters minimum")
+    password: z.string().min(8, "Password must be 10 characters minimum")
   });
   
   export type TsignUpConfirmSchema = z.infer<typeof signUpConfirmSchema>;
